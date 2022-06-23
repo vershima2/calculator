b3aed970d37d03ef0775272bd32d82d5c1b69b54
samson vershima saaikyaa <samsonsaaikyaa1@gmail.com>
2022-06-23 03:01:43 -0700

    Basic Javascript calculator task

---
+++ srcipt.js
@@ -0,0 +1,25 @@
+x=parseFloat(parseInt(prompt("Enter first number")));
+y=parseFloat(parseInt(prompt("Enter second number")));
+oper=prompt("Type an operator");
+
+if (oper== '+'){
+z = x+y;
+alert("sum:  " +z);
+} 
+else if (oper=='-'){
+    a = x-y
+    alert("Difference:  " +a);
+}
+else if(oper=='*'){
+    b = x*y
+    alert("Product:  " +b);
+}
+else if(oper=='/'){
+    c = x/y
+    alert("quotient:  " +c);
+}
+
+
+
+
+
